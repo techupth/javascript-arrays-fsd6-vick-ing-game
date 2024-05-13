2da29d4284c0b97af5759792a7e668ef5bafc2b5
jarudech <jarudech013@gmail.com>
2024-05-13 13:13:20 +0700

    did exercise 1-7

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,20 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = Array.isArray(orders);
+console.log(isArray);
+
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+console.log(creditCardTypeOfBlindermann);
+
+orders[2].creditCardType = "visa";
+
+let productQuantityOfJoannet = orders[3].productQuantity;
+console.log(productQuantityOfJoannet);
+
+let totalPurchaseOfDary = orders[1].productQuantity * orders[1].productPrice;
+console.log(totalPurchaseOfDary);
+
+orders.shift();
+
+console.log(orders);
